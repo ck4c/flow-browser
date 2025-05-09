b2c99bc8cdd8dde15e4429f0352b76b4e3e90464
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-05-09 08:03:17 +0100

    move: locales to shared folder

--- src/renderer/src/lib/i18n.ts
+++ src/renderer/src/lib/i18n.ts
@@ -3,11 +3,11 @@ import { initReactI18next, useTranslation } from "react-i18next";
 import resourcesToBackend from "i18next-resources-to-backend";
 
 // Tell vite to load all locale files
-import.meta.glob("@/locales/*/*.json");
+import.meta.glob("~/locales/*/*.json");
 
 // Initialize i18n as quickly as possible
 i18n
-  .use(resourcesToBackend((language: string, namespace: string) => import(`@/locales/${language}/${namespace}.json`)))
+  .use(resourcesToBackend((language: string, namespace: string) => import(`~/locales/${language}/${namespace}.json`)))
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     lng: "en", // default language
