4759fe0d069d18a04ddd7140f15822c1d803b6fb
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-05-15 16:46:11 +0100

    chore: improve style

--- src/renderer/src/components/browser-ui/sidebar/content/space-sidebar.tsx
+++ src/renderer/src/components/browser-ui/sidebar/content/space-sidebar.tsx
@@ -6,18 +6,16 @@ import { Button } from "@/components/ui/button";
 import { SidebarGroup, SidebarMenu } from "@/components/ui/resizable-sidebar";
 import { Space } from "@/lib/flow/interfaces/sessions/spaces";
 import { cn, hex_is_light } from "@/lib/utils";
-import { AnimatePresence, Reorder } from "motion/react";
+import { AnimatePresence, Reorder, motion } from "motion/react";
 import { useCallback, useMemo, useRef, useState } from "react";
 
-function SidebarSectionDivider({
-  showClearButton,
-  handleCloseAllTabs
-}: {
-  showClearButton: boolean;
-  handleCloseAllTabs: () => void;
-}) {
+const ENABLE_SECTION_DEVIDER = false;
+
+function SidebarSectionDivider({ hasTabs, handleCloseAllTabs }: { hasTabs: boolean; handleCloseAllTabs: () => void }) {
+  if (!hasTabs) return null;
+
   return (
-    <div
+    <motion.div
       className={cn(
         "flex flex-row",
         "items-center justify-between",
@@ -25,24 +23,26 @@ function SidebarSectionDivider({
         "h-1 gap-1",
         "mt-0" // mt-0 is temporary, just for right now before pinned tabs releases
       )}
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.2 }}
     >
       <div className={cn("h-[1px] flex-grow", "bg-black/10 dark:bg-white/25")} />
-      {showClearButton && (
-        <Button
-          className={cn(
-            "h-1 !p-1 rounded-sm",
-            "text-black/50 dark:text-white/50",
-            "hover:text-black/80 dark:hover:text-white/80",
-            "hover:bg-transparent hover:dark:bg-transparent"
-          )}
-          variant="ghost"
-          size="sm"
-          onClick={handleCloseAllTabs}
-        >
-          <span className="text-xs font-semibold">Clear</span>
-        </Button>
-      )}
-    </div>
+      <Button
+        className={cn(
+          "h-1 !p-1 rounded-sm",
+          "text-black/50 dark:text-white/50",
+          "hover:text-black/80 dark:hover:text-white/80",
+          "hover:bg-transparent hover:dark:bg-transparent"
+        )}
+        variant="ghost"
+        size="sm"
+        onClick={handleCloseAllTabs}
+      >
+        <span className="text-xs font-semibold">Clear</span>
+      </Button>
+    </motion.div>
   );
 }
 
@@ -84,14 +84,18 @@ export function SpaceSidebar({ space }: { space: Space }) {
 
   const [draggingTabGroup, setDraggingTabGroup] = useState<number | null>(null);
 
-  const showClearButton = tabGroups.length > 0;
+  const hasTabs = tabGroups.length > 0;
 
   return (
     <div className={cn(isSpaceLight ? "" : "dark", "h-full")} ref={sidebarRef}>
       <SpaceTitle space={space} />
-      <SidebarGroup>
+      <SidebarGroup className="py-0.5">
         <SidebarMenu>
-          <SidebarSectionDivider showClearButton={showClearButton} handleCloseAllTabs={handleCloseAllTabs} />
+          {ENABLE_SECTION_DEVIDER && (
+            <AnimatePresence>
+              {hasTabs && <SidebarSectionDivider hasTabs={hasTabs} handleCloseAllTabs={handleCloseAllTabs} />}
+            </AnimatePresence>
+          )}
           <NewTabButton />
           <Reorder.Group
             as="div"
@@ -100,7 +104,7 @@ export function SpaceSidebar({ space }: { space: Space }) {
             values={sortedTabGroups.map((tabGroup) => tabGroup.id)}
             axis="y"
           >
-            <div className="flex flex-col justify-between gap-0.5">
+            <div className="flex flex-col justify-between gap-1">
               <AnimatePresence initial={false}>
                 {sortedTabGroups.map((tabGroup) => (
                   <Reorder.Item
